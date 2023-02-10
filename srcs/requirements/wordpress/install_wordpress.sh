cfd86288a1f73f374f7e26debfa44e83831e863a
chelmerd <chelmerd@student.42wolfsburg.de>
2023-02-10 15:37:06 +0100

    install wordpress on container startup

---
+++ srcs/requirements/wordpress/install_wordpress.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+WP_CLI="wp-cli --allow-root"
+
+$WP_CLI core download
+
+echo configure wordpress...
+rm -f wp-config
+$WP_CLI core config \
+    --dbhost=${WORDPRESS_DB_HOST} \
+    --dbname=${WORDPRESS_DB_NAME} \
+    --dbuser=${WORDPRESS_DB_USER} \
+    --dbpass=${WORDPRESS_DB_PASSWORD} \
+    --skip-check
+
+echo install wordpress
+$WP_CLI core install \
+    --url=${DOMAIN_NAME} \
+    --title=${WORDPRESS_WEBSITE_TITLE} \
+    --admin_user=${WORDPRESS_ADMIN} \
+    --admin_password=${WORDPRESS_ADMIN_PASSWORD} \
+    --admin_email=${WORDPRESS_EMAIL}
